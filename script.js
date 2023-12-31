c85618195d53fb434974835c6de05e4e128596fa
Cheng <>
2023-12-31 14:52:46 +1000

    Add playRound function Add param case insensitive

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ function getComputerChoice(){
     }
 
     else if (randomInt==0){
-        return "Sicssors";
+        return "Scissors";
     }
+}
 
+function playRound(computerSelection,playerSelection){
+    let  playerAnswer = playerSelection.toLowerCase()
+    if (computerSelection == "Rock" && playerAnswer == "scissors"){
+        return "You lose. Rock beats scissors"
+    }
+    else if (computerSelection == "Rock" && playerAnswer == "paper"){
+        return "You win. Paper beats rock"
+    }
+    else if (computerSelection == "Paper" && playerAnswer == "rock"){
+        return "You lose. Paper beats rock."
+    }
+    else if (computerSelection =="Paper" && playerAnswer == "scissors"){
+        return "You win. Scissors beats paper"
+    }
+    else if (computerSelection =="Scissors" && playerAnswer =="paper"){
+        return "You lose. Sicssors beats paper"
+    }
+    else if (computerSelection =="Scissors" && playerAnswer =="rock"){
+        return "You win. Rock beats Sicssors "
+    }
+    else if (computerSelection == playerAnswer){
+        return playRound()
+    }
 }
