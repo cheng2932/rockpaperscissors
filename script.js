66309159bc579af4bdc7cf020b996dced173045a
Cheng <>
2024-01-02 21:00:45 +1000

    Finish rps game. -- Add human + computer scoring

--- script.js
+++ script.js
@@ -37,3 +37,47 @@ function playRound(playerSelection,computerSelection){
         return playRound(playerSelection,getComputerChoice())
     }
 }
+
+// pseudocode
+// FUNCTION game
+//  SET computerScore to 0
+//  SET playerScore to 0
+//  FOR 1 to 5 
+//      GET user input
+//      SET playerSelection to user input
+//      CALL playRound with playerSelection and getComputerchoice RETURNING string
+//      IF "YOU LOSE" 
+//          INCREMENT computerScore +1
+//      ENDIF 
+//      ELSE IF "YOU WIN" 
+//          INCREMENT playerScore +1
+//      ENDIF 
+//      DISPLAY computerSCore and playerScore
+//  ENDFOR
+// IF 
+// END FUNCTION
+
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i=0;i<5;i++){
+        let playerAnswer = String(prompt("Enter scissors, rock or paper"))
+        let win = playRound(playerAnswer,getComputerChoice());
+        console.log(win)
+        if (win.includes("win") == true){
+            playerScore += 1
+            console.log(`Your score is ${playerScore}`)
+        }
+        else{
+            computerScore +=1
+            console.log(`Computer score is ${computerScore}`)
+        }
+       
+    }
+    if (computerScore > playerScore){
+        console.log("Computer wins!")
+    }
+    else if (playerScore<computerScore){
+        console.log("Human wins!")
+    }
+}
